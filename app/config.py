89923ad325bca4b57c0d196111d5c0a6694eccbb
Sithira Akbo <56906001+dlsakbo@users.noreply.github.com>
2024-08-24 02:53:55 +1000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv(postgresql://sit722_part_1_3ik2_user:wZzM9Q9zjcL9K81ee6Kc0DX7sYEm0HYe@dpg-cr0tukjqf0us73ffs7i0-a.oregon-postgres.render.com/sit722_part_1_3ik2)
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
