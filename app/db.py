99f85583d4ecabe527cb60a6fcdb6d64b95cf34e
Sithira Akbo <56906001+dlsakbo@users.noreply.github.com>
2024-08-24 02:54:20 +1000

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from config import settings
 
 # SQLAlchemy database engine and session
-engine = create_engine(settings.postgresql://sit722_part_1_3ik2_user:wZzM9Q9zjcL9K81ee6Kc0DX7sYEm0HYe@dpg-cr0tukjqf0us73ffs7i0-a.oregon-postgres.render.com/sit722_part_1_3ik2)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
